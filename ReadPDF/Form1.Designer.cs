8ef8499690f411694f0952b9bc8b572db5c99c51
Neophear <stiiggade@gmail.com>
2019-10-11 10:38:26 +0200

    Initial commit

---
+++ ReadPDF/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ReadPDF
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtRegEx = new System.Windows.Forms.TextBox();
+            this.lblRegEx = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.ofdPDF = new System.Windows.Forms.OpenFileDialog();
+            this.lnklblRegexPal = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 100);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(559, 277);
+            this.txtOutput.TabIndex = 99;
+            // 
+            // txtRegEx
+            // 
+            this.txtRegEx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegEx.Location = new System.Drawing.Point(144, 40);
+            this.txtRegEx.Multiline = true;
+            this.txtRegEx.Name = "txtRegEx";
+            this.txtRegEx.Size = new System.Drawing.Size(346, 54);
+            this.txtRegEx.TabIndex = 2;
+            this.txtRegEx.Text = "\\+45\\d+.*\\d+,\\d+";
+            // 
+            // lblRegEx
+            // 
+            this.lblRegEx.AutoSize = true;
+            this.lblRegEx.Location = new System.Drawing.Point(12, 43);
+            this.lblRegEx.Name = "lblRegEx";
+            this.lblRegEx.Size = new System.Drawing.Size(126, 16);
+            this.lblRegEx.TabIndex = 2;
+            this.lblRegEx.Text = "Regular Expression";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(12, 15);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(47, 16);
+            this.lblFilePath.TabIndex = 4;
+            this.lblFilePath.Text = "Sti til fil";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.Location = new System.Drawing.Point(144, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(346, 22);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRun.Location = new System.Drawing.Point(496, 71);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Kør";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFile.Location = new System.Drawing.Point(496, 12);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFile.TabIndex = 1;
+            this.btnBrowseFile.Text = "Åben fil";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // ofdPDF
+            // 
+            this.ofdPDF.DefaultExt = "*.pdf";
+            this.ofdPDF.FileName = "*.pdf";
+            this.ofdPDF.Title = "Åben PDF";
+            this.ofdPDF.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdPDF_FileOk);
+            // 
+            // lnklblRegexPal
+            // 
+            this.lnklblRegexPal.AutoSize = true;
+            this.lnklblRegexPal.Location = new System.Drawing.Point(12, 59);
+            this.lnklblRegexPal.Name = "lnklblRegexPal";
+            this.lnklblRegexPal.Size = new System.Drawing.Size(120, 16);
+            this.lnklblRegexPal.TabIndex = 100;
+            this.lnklblRegexPal.TabStop = true;
+            this.lnklblRegexPal.Text = "www.regexpal.com";
+            this.lnklblRegexPal.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblRegexPal_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 389);
+            this.Controls.Add(this.lnklblRegexPal);
+            this.Controls.Add(this.btnBrowseFile);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblRegEx);
+            this.Controls.Add(this.txtRegEx);
+            this.Controls.Add(this.txtOutput);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "ReadPDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtRegEx;
+        private System.Windows.Forms.Label lblRegEx;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.OpenFileDialog ofdPDF;
+        private System.Windows.Forms.LinkLabel lnklblRegexPal;
+    }
+}
+
---
+++ ReadPDF/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace ReadPDF
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            ofdPDF.ShowDialog();
+        }
+
+        private void ofdPDF_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFilePath.Text = ofdPDF.FileName;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+
+            if (File.Exists(txtFilePath.Text))
+            {
+                try
+                {
+                    StringBuilder text = new StringBuilder();
+                    Regex re = new Regex(txtRegEx.Text);
+
+                    using (PdfReader reader = new PdfReader(File.OpenRead(txtFilePath.Text)))
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                            foreach (Match m in re.Matches(PdfTextExtractor.GetTextFromPage(reader, i)))
+                                text.Append(m.Value + Environment.NewLine);
+
+                    txtOutput.Text = text.ToString();
+                }
+                catch (BadPdfFormatException)
+                {
+                    MessageBox.Show("PDF-filen kunne ikke læses");
+                }
+            }
+            else
+                MessageBox.Show("Filen findes ikke");
+        }
+
+        private void lnklblRegexPal_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(lnklblRegexPal.Text);
+        }
+    }
+}
